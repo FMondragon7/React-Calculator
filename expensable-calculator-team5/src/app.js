231360ce6f073ea5ea72d41673c3b6ba82e95472
FMondragon7 <fernandomondragon07@gmail.com>
2023-06-17 16:06:22 -0700

    feat: add calculator output section

--- expensable-calculator-team5/src/app.js
+++ expensable-calculator-team5/src/app.js
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import Calculator from "./components/calculator";
 
-const calculatorStyle = css`
+const appStyle = css`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -22,11 +23,75 @@ const addExpenseStyle = css`
   flex-grow: 0;
 `;
 
+const expenseDescriptionStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  position: absolute;
+  width: 104px;
+  height: 40px;
+  left: 60px;
+  top: 5px;
+`;
+
+const expenseP1Style = css`
+  width: 104px;
+  height: 16px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: #ffffff;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const expenseP2Style = css`
+  width: 104px;
+  height: 24px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+const calculatorStyle = css`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 1px;
+  gap: 1px;
+  width: 256px;
+  height: 283px;
+  background: #e5e7eb;
+  border: 1px solid #e5e7eb;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
 function App() {
   return (
-    <div css={calculatorStyle}>
-      <div css={addExpenseStyle}>Add expense to Groseries</div>
-    </div>
+    <main css={appStyle}>
+      <section css={addExpenseStyle}>
+        <div css={expenseDescriptionStyle}>
+          <label css={expenseP1Style}>Add expense to</label>
+          <label css={expenseP2Style}>Groceries</label>
+        </div>
+      </section>
+      <section css={calculatorStyle}>
+        <Calculator />
+      </section>
+    </main>
   );
 }
 
---
+++ expensable-calculator-team5/src/components/calculator.js
@@ -0,0 +1,74 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const outputStyle = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 11px 20px;
+  gap: 10px;
+  width: 254px;
+  height: 50px;
+  background: #ffffff;
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
+`;
+
+const outputLabelStyle = css`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 4px;
+  width: 30px;
+  height: 28px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const dollarStyle = css`
+  width: 13px;
+  height: 28px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  text-align: center;
+  color: #4b5563;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+const resultStyle = css`
+  width: 13px;
+  height: 28px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  text-align: center;
+  color: #4b5563;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+function Calculator() {
+  return (
+    <div css={outputStyle}>
+      <div css={outputLabelStyle}>
+        <label css={dollarStyle}>$</label>
+        <label css={resultStyle}>1</label>{" "}
+        {/* pending to make result stay center */}
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
