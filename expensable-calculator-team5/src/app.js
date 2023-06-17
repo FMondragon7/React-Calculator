ecf6606fd3a02bca7f74d74505bc9cc0a9712919
FMondragon7 <fernandomondragon07@gmail.com>
2023-06-17 15:21:53 -0700

    feat: add header for add expense

--- expensable-calculator-team5/src/app.js
+++ expensable-calculator-team5/src/app.js
@@ -1,5 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const calculatorStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  width: 256px;
+  height: 333px;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const addExpenseStyle = css`
+  width: 256px;
+  height: 50px;
+  background: #06b6d4;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
 function App() {
-  return <h1> welcome team 5 to calculator! </h1>;
+  return (
+    <div css={calculatorStyle}>
+      <div css={addExpenseStyle}>Add expense to Groseries</div>
+    </div>
+  );
 }
 
 export default App;
